c6b8fa7e3eda4f7b30309920e1540ec93baa70eb
reinhake <reinhake@oregonstate.edu>
2022-01-23 22:58:50 -0800

    fourth test

--- tests.py
+++ tests.py
@@ -2,11 +2,9 @@ import unittest
 from credit_card_validator import credit_card_validator
 
 class TestCase(unittest.TestCase):
-
-
     # verifies if American Express with valid langths and valid check bits returns True
     # Picked using Category Partition Testing
-    def test2(self):
+    def test1(self):
         a = 349357573124702
         self.assertTrue(credit_card_validator(a))
     
@@ -24,9 +22,10 @@ class TestCase(unittest.TestCase):
         
     # verifies if American Express with valid langths and invalid check bits returns False
     # Picked using Category Partition Testing
-    def test1(self):
+    def test4(self):
         a = 349094094095321
         self.assertTrue(credit_card_validator(a))
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
