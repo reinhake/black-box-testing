d4fe70a99ecc6ae39fb713e78ba67e0b874c8c7b
reinhake <reinhake@oregonstate.edu>
2022-01-23 23:26:11 -0800

    ninth test

--- tests.py
+++ tests.py
@@ -46,25 +46,48 @@ class TestCase(unittest.TestCase):
         self.assertFalse(credit_card_validator(a))
 
 
-    # verifies if a card with invalid length returns False
+    # verifies if a card with invalid starting bits returns False
     # Picked using Category Partition Testing
     def test7(self):
-        a = 55742367048253
+        a = 237423670482533
         self.assertFalse(credit_card_validator(a))
 
 
-    # verifies if a card with invalid length returns False
+    # verifies if a card with invalid starting bits returns False
     # Picked using Category Partition Testing
     def test8(self):
-        a = 55742367048215128
-        self.assertTrue(credit_card_validator(a))
+        a = 23742367048215128
+        self.assertFalse(credit_card_validator(a))
     
 
     # verifies if a card with invalid length returns False
     # Picked using Category Partition Testing
     def test9(self):
         a = 0
-        self.assertTrue(credit_card_validator(a))
+        self.assertFalse(credit_card_validator(a))
+
+
+    # verifies if a American Express with invalid length returns False
+    # Picked using Category Partition Testing
+    def test10(self):
+        a = 3493575731247023
+        self.assertFalse(credit_card_validator(a))
+
+
+    # verifies if a Visa with invalid length returns False
+    # Picked using Category Partition Testing
+    def test11(self):
+        a = 406570033553287
+        self.assertFalse(credit_card_validator(a))
+
+    
+    # verifies if a MasterCard with invalid length returns False
+    # Picked using Category Partition Testing
+    def test12(self):
+        a = 537207584445482
+        self.assertFalse(credit_card_validator(a))
+
+    
 
 
 if __name__ == '__main__':
