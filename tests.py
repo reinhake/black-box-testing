7a02532014ebc119c923c98b9d6ffa356f4597e8
reinhake <reinhake@oregonstate.edu>
2022-01-23 22:50:09 -0800

    second test

--- tests.py
+++ tests.py
@@ -2,8 +2,14 @@ import unittest
 from credit_card_validator import credit_card_validator
 
 class TestCase(unittest.TestCase):
+    #verifies if MasterCard with valid langths and invalid check bits returns False
+    #Picked using Category Partition Testing
     def test1(self):
         a = 349094094095321
-        self.assertFalse(credit_card_validator(a))
+        self.assertTrue(credit_card_validator(a))
+    
+    def test2(self):
+        a = 4521009765305084
+        self.assertTrue(credit_card_validator(a))
 if __name__ == '__main__':
   unittest.main(verbosity=2)
