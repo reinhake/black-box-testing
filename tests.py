9b1fe4827ab73bdd7d7e46fe5ccc0b5f00a18776
reinhake <reinhake@oregonstate.edu>
2022-01-23 22:36:17 -0800

    first test

--- tests.py
+++ tests.py
@@ -1,2 +1,9 @@
+import unittest
 from credit_card_validator import credit_card_validator
 
+class TestCase(unittest.TestCase):
+    def test1(self):
+        a = 349094094095321
+        self.assertFalse(credit_card_validator(a))
+if __name__ == '__main__':
+  unittest.main(verbosity=2)
