ac3a03d824083c8e5e8f231fd21f0558fb15c116
reinhake <reinhake@oregonstate.edu>
2022-01-23 23:10:04 -0800

    fifth test

--- tests.py
+++ tests.py
@@ -2,30 +2,55 @@ import unittest
 from credit_card_validator import credit_card_validator
 
 class TestCase(unittest.TestCase):
-    # verifies if American Express with valid langths and valid check bits returns True
+    # verifies if American Express with valid lengths and valid check bits returns True
     # Picked using Category Partition Testing
     def test1(self):
         a = 349357573124702
         self.assertTrue(credit_card_validator(a))
     
-    # verifies if Visa with valid langths and valid check bits returns True
+
+    # verifies if Visa with valid lengths and valid check bits returns True
     # Picked using Category Partition Testing
     def test2(self):
-        a = 4521009765305084
+        a = 4916822882163365
         self.assertTrue(credit_card_validator(a))
 
-    # verifies if MasterCard with valid langths and valid check bits returns True
+
+    # verifies if MasterCard with valid lengths and valid check bits returns True
     # Picked using Category Partition Testing
     def test3(self):
         a = 5372075184720507
         self.assertTrue(credit_card_validator(a))
         
-    # verifies if American Express with valid langths and invalid check bits returns False
+
+    # verifies if American Express with valid lengths and invalid check bits returns False
     # Picked using Category Partition Testing
     def test4(self):
         a = 349094094095321
         self.assertTrue(credit_card_validator(a))
 
 
+    # verifies if Visa with valid lengths and invalid check bits returns False
+    # Picked using Category Partition Testing
+    def test5(self):
+        a = 4521009765305081
+        self.assertTrue(credit_card_validator(a))
+
+    
+    # verifies if Master Card with valid lengths and invalid check bits returns False
+    # Picked using Category Partition Testing
+    def test6(self):
+        a = 5574236704821512
+        self.assertTrue(credit_card_validator(a))
+
+
+    # verifies if a card with invalid length returns False
+    # Picked using Category Partition Testing
+    def test7(self):
+        a = 557423670482
+        self.assertTrue(credit_card_validator(a))
+
+
 if __name__ == '__main__':
     unittest.main(verbosity=2)
+    
